5aeeefd85262e55de1013a3b5b3282c7d730a503
ping <913476332@qq.com>
2016-03-07 18:42:20 +0800

    第er次版本提交

---
+++ src/java/org/apache/sqoop/SqoopVersion.java
@@ -0,0 +1,12 @@
+package org.apache.sqoop;
+
+public class SqoopVersion {
+	public final String VERSION="1.4.6";
+
+	@Override
+	public String toString() {
+		return "SqoopVersion [VERSION=" + VERSION + "]";
+	}
+	
+
+}
